7427023ccb133c34e02a813317a8a193cb4da945
scruff mcscruffington <baronesse.poubelle@gmail.com>
2013-07-11 00:31:13 +0200

    did some work on documentation, added bibliography

--- documentation/build.sh
+++ documentation/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 pdflatex documentation.tex
 pdflatex documentation.tex
+bibtex documentation.aux
+pdflatex documentation.tex
 rm -f *.aux *.log *.nav *.out *.snm *.toc *.blg *.bbl .log
 
 notify-send -t 3000  "pdflatex: " "$(date +%H:%M) done!"
